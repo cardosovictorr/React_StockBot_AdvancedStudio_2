f23dc21c637b4a82aad968dba60754131bc207d6
cardosovictorr <62979369+cardosovictorr@users.noreply.github.com>
2021-03-19 13:19:47 +1100

    adding pages files and organizing folder

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Navbar from "./components/Navbar/Navbar"; 
-import Footer from  "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import './Login.css';
 
 function Login() {
   return (
     <div className="Login">
       <Navbar />
-   
+
     </div>
   );
 }
